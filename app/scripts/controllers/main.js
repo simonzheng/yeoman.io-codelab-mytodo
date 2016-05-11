38e6ff71a9763f09197ca6ec3a1a502f191704a9
Simon Zheng <simonz@palantir.com>
2016-05-11 09:39:49 -0400

    Change to static TODO app with ng-repeat (no ability to add yet)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,6 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name mytodoApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the mytodoApp
- */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
   });
